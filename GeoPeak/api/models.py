b2c0452ec3649be6d4bd8af3b6bb5701cf87c313
t.cassan <t.cassan@mobiyo.com>
2021-10-08 08:08:15 +0200

    creation of Peak model and test view

--- GeoPeak/api/models.py
+++ GeoPeak/api/models.py
@@ -1,3 +1,12 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Peak(models.Model):
+    name = models.CharField(max_length=200)
+    lat = models.FloatField(validators=[MinValueValidator(-90), MaxValueValidator(90)])
+    long = models.FloatField(validators=[MinValueValidator(-180), MaxValueValidator(180)])
+    altitude = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.name} ({self.altitude})"
