167b6edbad493b87e73f9a2a07cd87f8847f4367
t.cassan <t.cassan@mobiyo.com>
2021-10-10 18:27:52 +0200

    doc OK

--- GeoPeak/api/urls.py
+++ GeoPeak/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from rest_framework.documentation import include_docs_urls
 
 from . import views
 
 urlpatterns = [
     path("peak", views.list_peak),
     path("peak/<int:pk>", views.detail_peak),
-    path("upload_list_peak", views.upload_list_peak)
+    path("upload_list_peak", views.upload_list_peak),
+    path('docs/', include_docs_urls(title='Peak API'))
 ]
